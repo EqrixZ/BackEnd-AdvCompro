ef9abdbae992b78bdcd641df38ccc85b3f78bb1e
EqrixZ <s6606022630049@email.kmutnb.ac.th>
2024-09-11 11:32:21 +0700

    BackEnd

--- src/CRUDBookSQLite.js
+++ src/CRUDBookSQLite.js
@@ -70,7 +70,7 @@ app.delete('/books/:id', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
